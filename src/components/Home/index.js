50c7750e258c8984229024a9346cff1718479df5
tarun <btaruntej143@gmail.com>
2024-03-09 09:17:59 +0530

    BugFixes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,71 @@
+// CLASS COMPONENT
 import { Component } from "react";
 import SocialMenu from "../SocialMenu";
+import {
+  PROFILE_IMG_URL,
+  QUOTES_LIST,
+  WHATSAPP_URL,
+  RESUME_DOWNLOAD_LINK,
+} from "../../utils/constants";
 import "./index.css";
-
-const quotes = [
-  "Programming is the closest thing we have to magic spells.",
-  "It's not a bug, it's an undocumented feature.",
-  "Debugging is like hunting for aliens in a haystack.",
-];
 
 class Home extends Component {
   state = { activeQuote: "" };
 
   onClickGenerateButton = () => {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    this.setState({ activeQuote: quotes[randomIndex] });
+    const randomIndex = Math.floor(Math.random() * QUOTES_LIST.length);
+    this.setState({ activeQuote: QUOTES_LIST[randomIndex] });
   };
   componentDidMount() {
     this.onClickGenerateButton();
+  }
+
+  renderProfileIntroductionAndImageSection() {
+    return (
+      <div className="profile-details-block">
+        <div className="profile-message-block">
+          <h1 className="profile-message-text">hy,Hello!</h1>
+          <h1 className="profile-name-text">
+            I'm <span className="my-name">Tarun Bommali </span>
+          </h1>
+          <p className="job-role">~ Software Devloper</p>
+        </div>
+        <button
+          className="quote-generate-button "
+          type="button"
+          onClick={this.onClickGenerateButton}
+        >
+          <img src={PROFILE_IMG_URL} alt="profile" className="profile-image" />
+        </button>
+      </div>
+    );
+  }
+
+  renderContactAndResumeSection() {
+    return (
+      <div className="contact-block">
+        <div className="contact-buttons-block">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="contact-btn-a-link"
+            href={WHATSAPP_URL}
+          >
+            <button className="contact-btn-link">Hire Me</button>
+          </a>
+
+          <a
+            target="_blank"
+            className="contact-btn-a-link"
+            rel="noreferrer"
+            href={RESUME_DOWNLOAD_LINK}
+          >
+            <button className="contact-btn-link"> Download CV</button>
+          </a>
+        </div>
+        <SocialMenu />
+      </div>
+    );
   }
 
   render() {
@@ -24,55 +73,9 @@ class Home extends Component {
     return (
       <div className="home-container">
         <p className="text-quote">" {activeQuote}</p>
-
         <div className="profile-section">
-          <div className="profile-and-name">
-            <div className="hello-box">
-              <h1 className="message">hy,Hello!</h1>
-              <h1 className="name">
-                I'm <span className="my-name">Tarun Bommali </span>
-              </h1>
-              <p className="job-role">~ Software Devloper</p>
-            </div>
-            <button
-              className="text-quote-button "
-              type="button"
-              onClick={this.onClickGenerateButton}
-            >
-              <img
-                src="https://res.cloudinary.com/drdgj0pch/image/upload/v1707662912/WhatsApp_Image_2024-02-09_at_14.26.09_9a0ddc7f_yqjui9.jpg"
-                alt="profile"
-                className="profile"
-              />
-            </button>
-          </div>
-          <div className="connect-box">
-            <div className="button-container">
-              <button className="hire-button btn-link">
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  className="a-link a-link-hire-btns"
-                  href={`whatsapp://send?phone=+91 9581193026&text=${encodeURIComponent(
-                    "I'm contacting you through your website."
-                  )}`}
-                >
-                  Hire Me
-                </a>
-              </button>
-              <button className="cv-button btn-link">
-                <a
-                  target="_blank"
-                  className="a-link a-link-hire-btns"
-                  rel="noreferrer"
-                  href="https://docs.google.com/document/d/13YS7DEFjv5MNSksfE4flMRyalAcxU3CpG5O7pLTbFJI/edit?usp=sharing"
-                >
-                  Download CV
-                </a>
-              </button>
-            </div>
-            <SocialMenu />
-          </div>
+          {this.renderProfileIntroductionAndImageSection()}
+          {this.renderContactAndResumeSection()}
         </div>
       </div>
     );
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -12,64 +12,69 @@ export default class Skills extends Component {
     this.setState({ activeTopic: topic });
   };
 
-  render() {
+  renderSkillsList() {
+    return (
+      <ul className="skills-list">
+        {skillsList.map(({ id, technology, icon, rating }) => (
+          <button
+            className="skill-item-button"
+            onClick={() => this.onClickSkillItem(technology)}
+            key={id}
+          >
+            <li className="skill-item">
+              <div className="skill-item-header">
+                {icon}
+                <div className="skill-rating-container">
+                  <p className="skill-title">{technology}</p>
+                  <p>
+                    {Array(5)
+                      .fill(null)
+                      .map((_, index) => (
+                        <FaStar
+                          key={index}
+                          className={
+                            index < rating ? "default-star" : "active-star"
+                          }
+                        />
+                      ))}
+                  </p>
+                </div>
+              </div>
+            </li>
+          </button>
+        ))}
+      </ul>
+    );
+  }
+
+  renderSkillItemConcepts() {
     const { activeTopic } = this.state;
     return (
-      <div className="skills-container">
-        {/* Skills Section */}
-        <div className="skill-section-concepts">
-          <div className="skills-section">
-            <ul className="skills-list">
-              {skillsList.map(({ id, technology, icon, rating }) => (
-                <button
-                  className="skill-item-button"
-                  onClick={() => this.onClickSkillItem(technology)}
-                  key={id}
-                >
-                  <li className="skill-item">
-                    <div className="skill-item-header">
-                      {icon}
-                      <div className="skill-rating-container">
-                        <p className="skill-title">{technology}</p>
-                        <p>
-                          {Array(5)
-                            .fill(null)
-                            .map((_, index) => (
-                              <FaStar
-                                key={index}
-                                className={
-                                  index < rating
-                                    ? "default-star"
-                                    : "active-star"
-                                }
-                              />
-                            ))}
-                        </p>
-                      </div>
-                    </div>
+      <div className="skills-concepts">
+        {skillsList
+          .filter(({ technology }) => technology === activeTopic)
+          .map(({ id, technology, keyTopics }) => (
+            <div key={id}>
+              <h3>{technology}</h3>
+              <ul className="topic-list">
+                {Object.keys(keyTopics).map((topic, index) => (
+                  <li className="topic-item" key={index}>
+                    {topic}
                   </li>
-                </button>
-              ))}
-            </ul>
-          </div>
+                ))}
+              </ul>
+            </div>
+          ))}
+      </div>
+    );
+  }
 
-          {/* Filtered Topic List */}
-          <div className="skills-concepts">
-            {skillsList
-              .filter(({ technology }) => technology === activeTopic)
-              .map(({ id, technology, keyTopics }) => (
-                <div key={id}>
-                  <h3>{technology}</h3>
-                  <ul className="topic-list">
-                    {Object.keys(keyTopics).map((topic, index) => (
-                      <li className="topic-item" key={index}>
-                        {topic}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ))}
-          </div>
+  render() {
+    return (
+      <div className="skills-container">
+        <div className="skill-section-concepts">
+          {this.renderSkillsList()}
+          {this.renderSkillItemConcepts()}
         </div>
         <SkillsFooter />
       </div>
